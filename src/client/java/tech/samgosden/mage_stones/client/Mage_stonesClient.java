597b8a58a275ecfc8cbc361cc9c545a8f7ea2eb6
Samuel James Gosden <samueljgosden@icloud.com>
2024-08-12 23:51:32 +0100

    Initial commit

---
+++ src/client/java/tech/samgosden/mage_stones/client/Mage_stonesClient.java
@@ -0,0 +1,11 @@
+package tech.samgosden.mage_stones.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Mage_stonesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/tech/samgosden/mage_stones/Mage_stones.java
@@ -0,0 +1,10 @@
+package tech.samgosden.mage_stones;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mage_stones implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
