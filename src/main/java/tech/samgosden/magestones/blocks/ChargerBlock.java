dd4bdcf60bd53d06e424311c97e6ac2c498dd0dc
rja46 <rjanderson15506@gmail.com>
2024-08-13 17:28:54 +0100

    Added charger block - incomplete

--- src/main/java/tech/samgosden/magestones/blocks/ChargerBlock.java
+++ src/main/java/tech/samgosden/magestones/blocks/ChargerBlock.java
@@ -2,11 +2,6 @@ package tech.samgosden.magestones.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
