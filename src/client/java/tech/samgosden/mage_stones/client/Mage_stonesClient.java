00b4c1134d137086e5b8b61f3004445f52b17a20
Samuel James Gosden <samueljgosden@icloud.com>
2024-08-13 01:09:46 +0100

    Add documentation

--- src/client/java/tech/samgosden/mage_stones/client/Mage_stonesClient.java
+++ src/client/java/tech/samgosden/mage_stones/client/Mage_stonesClient.java
@@ -1,26 +1,31 @@
 package tech.samgosden.mage_stones.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.text.Text;
 
 public class Mage_stonesClient implements ClientModInitializer {
+    /**
+     * This method is called when the client mod is initialized.
+     * It registers a listener for the JOIN event of the ClientPlayConnectionEvents.
+     * When the event is triggered, it retrieves the client player entity and sends a disclaimer message to the player's chat.
+     */
     @Override
     public void onInitializeClient() {
-        // Get the client instance
+        // Register a listener for the JOIN event of the ClientPlayConnectionEvents
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
 
-            // Get the client player entity
+            // Get the client player entity from the client instance
             ClientPlayerEntity player = client.player;
 
-            // Check if the player is not null
+            // Check if the player is not null to avoid NullPointerException
             if (player != null) {
                 // Create a text message for the in-development disclaimer
                 Text disclaimer = Text.literal("This mod is currently in development. Use with caution.");
 
                 // Send the disclaimer message to the player's chat
+                // The second parameter is set to false in order to prevent the message from being logged to the console
                 player.sendMessage(disclaimer, false);
             }
         });
