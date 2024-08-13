f25a8e5d307fe76d3871fbbecc78d584c6ac8ad3
rja46 <rjanderson15506@gmail.com>
2024-08-13 21:42:45 +0100

    Made an attempt to make light stones tickable

--- src/main/java/tech/samgosden/magestones/blocks/LightCrystalBlock.java
+++ src/main/java/tech/samgosden/magestones/blocks/LightCrystalBlock.java
@@ -1,9 +1,21 @@
 package tech.samgosden.magestones.blocks;
-
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
 
 public class LightCrystalBlock extends MageCrystalBlock {
+    int ticksLeft = 100;
     public LightCrystalBlock(int height, int xzOffset, AbstractBlock.Settings settings) {
         super(height, xzOffset, settings);
+    }
+
+    public void tick(World world, BlockPos pos, BlockState state, Random random) {
+        ticksLeft--;
+        System.out.println("ticksLeft: " + ticksLeft);
+        if (ticksLeft <= 0) {
+            world.setBlockState(pos, ModBlocks.DULL_MAGE_STONE_CRYSTAL.getDefaultState());
+        }
     }
 }
