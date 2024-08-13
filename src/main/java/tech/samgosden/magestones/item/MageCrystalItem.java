069c1c49c07619a87559d4f323be6541fdaee07d
Samuel James Gosden <samueljgosden@icloud.com>
2024-08-13 17:36:18 +0100

    Add rotation to BlockPlacement

--- src/main/java/tech/samgosden/magestones/item/MageCrystalItem.java
+++ src/main/java/tech/samgosden/magestones/item/MageCrystalItem.java
@@ -2,10 +2,13 @@ package tech.samgosden.magestones.item;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import tech.samgosden.magestones.blocks.ModBlocks;
 
@@ -20,9 +23,15 @@ public class MageCrystalItem extends Item {
     public ActionResult useOnBlock(ItemUsageContext context) {
         World world = context.getWorld();
         BlockPos pos = context.getBlockPos().offset(context.getSide());
-        BlockState blockState =  block.getDefaultState();
+        BlockState blockState = block.getDefaultState();
 
         if (world.isAir(pos)) {
+            PlayerEntity player = context.getPlayer();
+            if (player != null) {
+                Direction facing = player.getHorizontalFacing();
+                blockState = blockState.with(Properties.HORIZONTAL_FACING, facing);
+            }
+
             world.setBlockState(pos, blockState);
             context.getStack().decrement(1); // Decrease item count by 1
             return ActionResult.SUCCESS;
