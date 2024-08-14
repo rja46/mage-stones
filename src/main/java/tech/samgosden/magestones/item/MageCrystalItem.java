318b40a5a3bd74363defbdd8f744441a67a722f6
Samuel James Gosden <samueljgosden@icloud.com>
2024-08-14 22:35:27 +0100

    Update Durability logic

--- src/main/java/tech/samgosden/magestones/item/MageCrystalItem.java
+++ src/main/java/tech/samgosden/magestones/item/MageCrystalItem.java
@@ -2,7 +2,8 @@ package tech.samgosden.magestones.item;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
-
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 
 
 public class MageCrystalItem extends BlockItem {
@@ -15,5 +16,24 @@ public class MageCrystalItem extends BlockItem {
         return true; // This makes the item damageable
     }
 
+    @Override
+    public boolean isItemBarVisible(ItemStack stack) {
+        return stack.hasNbt() && stack.getNbt().contains("durability");
+    }
+
+    @Override
+    public int getItemBarStep(ItemStack stack) {
+        if (stack.hasNbt() && stack.getNbt().contains("durability")) {
+            int durability = stack.getNbt().getInt("durability");
+            int maxDurability = stack.getMaxDamage(); // Replace with your max durability
+            return Math.round(13.0F - (float) (maxDurability-durability) * 13.0F / (float) maxDurability);
+        }
+        return super.getItemBarStep(stack);
+    }
+
+    @Override
+    public int getItemBarColor(ItemStack stack) {
+        return MathHelper.hsvToRgb(Math.max(0.0F, (float) stack.getDamage() / (float) stack.getMaxDamage()) / 3.0F, 1.0F, 1.0F);
+    }
 
 }
