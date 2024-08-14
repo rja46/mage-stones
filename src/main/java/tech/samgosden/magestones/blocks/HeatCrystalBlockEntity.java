77a3894ca107ed87536eed92b3107396df3cefb1
rja46 <rjanderson15506@gmail.com>
2024-08-14 23:06:34 +0100

    Made the heat crystal smelt blocks

--- src/main/java/tech/samgosden/magestones/blocks/HeatCrystalBlockEntity.java
+++ src/main/java/tech/samgosden/magestones/blocks/HeatCrystalBlockEntity.java
@@ -3,10 +3,17 @@ package tech.samgosden.magestones.blocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.recipe.SmeltingRecipe;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import tech.samgosden.magestones.item.ModItems;
 import tech.samgosden.magestones.util.Util;
+
+import java.util.Optional;
 
 public class HeatCrystalBlockEntity extends MageCrystalBlockEntity {
     public HeatCrystalBlockEntity(BlockPos pos, BlockState state) {
@@ -24,8 +31,22 @@ public class HeatCrystalBlockEntity extends MageCrystalBlockEntity {
                      entity.damage(world.getDamageSources().inFire(), 2.0F);
                 }
 
-                ItemEntity[] items = Util.getDroppedItemEntitiesInRange(radiusSquared, world, pos);
+                ItemEntity[] items = Util.getDroppedItemEntitiesInRange(radius, world, pos);
+                for (ItemEntity item : items) {
+                    RecipeManager recipeManager = world.getRecipeManager();
 
+                    // Check if the item can be smelted
+                    Optional<SmeltingRecipe> smeltingRecipe = recipeManager.getFirstMatch(RecipeType.SMELTING, new SimpleInventory(item.getStack()), world);
+
+                    if (smeltingRecipe.isPresent()) {
+                        ItemStack itemStack = smeltingRecipe.get().getOutput(world.getRegistryManager());
+                        itemStack.setCount(item.getStack().getCount());
+                        world.spawnEntity(new ItemEntity(world, item.getX(), item.getY(), item.getZ(), itemStack ));
+                        item.kill();
+                    } else {
+                        System.out.println("The item cannot be smelted.");
+                    }
+                }
 
 
                 blockEntity.ticksLeft -= 1;
