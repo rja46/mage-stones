4d4620dbd1ba7e4812ade55357536c259a7d9700
Samuel James Gosden <samueljgosden@icloud.com>
2024-08-15 13:55:54 +0200

    Fix crash on spawn when block suppressing

--- src/main/java/tech/samgosden/magestones/blocks/GhoulCrystalBlockEntity.java
+++ src/main/java/tech/samgosden/magestones/blocks/GhoulCrystalBlockEntity.java
@@ -14,7 +14,7 @@ public class GhoulCrystalBlockEntity extends MageCrystalBlockEntity {
 
     public void onMobDeath(LivingEntity entity) {
         // Handle mob death near this block entity
-        if (!(entity instanceof ZombieEntity)) {
+        if (!(entity instanceof ZombieEntity)&&world!=null) {
             ZombieEntity zombie = new ZombieEntity(EntityType.ZOMBIE, entity.getEntityWorld());
             //0.1 is added to Y to stop it spawning inside a block.
             zombie.setPos(entity.getX(), entity.getY() + 0.1, entity.getZ());
