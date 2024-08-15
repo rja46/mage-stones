a822ff32f0464f8f27f93761914680bbdf70514c
rja46 <rjanderson15506@gmail.com>
2024-08-15 20:32:35 +0100

    Updated ghoul crystal

--- src/main/java/tech/samgosden/magestones/blocks/GhoulCrystalBlockEntity.java
+++ src/main/java/tech/samgosden/magestones/blocks/GhoulCrystalBlockEntity.java
@@ -1,24 +1,73 @@
 package tech.samgosden.magestones.blocks;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.SkeletonEntity;
+import net.minecraft.entity.mob.StrayEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeKeys;
 import tech.samgosden.magestones.item.ModItems;
+import tech.samgosden.magestones.util.Util;
+
+import java.util.Optional;
+import java.util.Random;
 
 public class GhoulCrystalBlockEntity extends MageCrystalBlockEntity {
     public GhoulCrystalBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntities.GHOUL_CRYSTAL_BLOCK_ENTITY, pos, state, ModItems.GHOUL_MAGE_STONE);
     }
 
+    public static void tick(World world, BlockPos pos, BlockState state, GhoulCrystalBlockEntity blockEntity) {
+        if (!world.isClient) {
+            if (blockEntity.ticksLeft > 0) {
+                int radius = blockEntity.effectRadius;
+                LivingEntity[] entities = Util.getLivingEntitiesInRange(radius, world, pos);
+                for (LivingEntity entity : entities) {
+                    if (!entity.isUndead()){
+                    entity.damage(world.getDamageSources().magic(), 1.0F);
+                    }
+                }
+                blockEntity.ticksLeft -= 1;
+            }
+            if (blockEntity.ticksLeft == 0) {
+                world.setBlockState(pos, state.with(GhoulCrystalBlock.ISACTIVE, false));
+            }
+        }
+    }
+
     public void onMobDeath(LivingEntity entity) {
         // Handle mob death near this block entity
-        if (!(entity instanceof ZombieEntity)&&world!=null) {
-            ZombieEntity zombie = new ZombieEntity(EntityType.ZOMBIE, entity.getEntityWorld());
-            //0.1 is added to Y to stop it spawning inside a block.
-            zombie.setPos(entity.getX(), entity.getY() + 0.1, entity.getZ());
-            world.spawnEntity(zombie);
+        if (ticksLeft > 0) {
+            if (!(entity.isUndead()) && world != null) {
+                Random random = new Random();
+                Entity undead;
+                if (random.nextBoolean()) {
+                    if (world.getBiome(entity.getBlockPos()).matchesKey(BiomeKeys.DESERT)) {
+                        undead = new ZombieEntity(EntityType.HUSK, entity.getEntityWorld());
+                    } else {
+                        undead = new ZombieEntity(EntityType.ZOMBIE, entity.getEntityWorld());
+                    }
+                } else {
+                    if (world.getBiome(entity.getBlockPos()).matchesKey(BiomeKeys.SNOWY_PLAINS)) {
+                        undead = new StrayEntity(EntityType.STRAY, entity.getEntityWorld()) {
+                        };
+                    } else if (world.getBiome(entity.getBlockPos()).matchesKey(BiomeKeys.ICE_SPIKES)) {
+                        undead = new StrayEntity(EntityType.STRAY, entity.getEntityWorld());
+                    } else if (world.getBiome(entity.getBlockPos()).matchesKey(BiomeKeys.FROZEN_RIVER)) {
+                        undead = new StrayEntity(EntityType.STRAY, entity.getEntityWorld());
+                    } else {
+                        undead = new SkeletonEntity(EntityType.SKELETON, entity.getEntityWorld());
+                    }
+                }
+
+                //0.1 is added to Y to stop it spawning inside a block.
+                undead.setPos(entity.getX(), entity.getY() + 0.1, entity.getZ());
+                world.spawnEntity(undead);
+            }
         }
     }
 }
