12d27a634d35e2dd132b3d6c7406c4ce915d6559
rja46 <rjanderson15506@gmail.com>
2024-08-16 19:19:08 +0100

    Energy is transferred, but only fully implemented for cold crystals, and the crystal is not visually reactivated.

--- src/main/java/tech/samgosden/magestones/blocks/CrystalConnectorEntity.java
+++ src/main/java/tech/samgosden/magestones/blocks/CrystalConnectorEntity.java
@@ -38,7 +38,37 @@ public class CrystalConnectorEntity extends BlockEntity {
             }
             if (crystalBlock != null){
                 if (crystalBlock.isReceivingEnergy()){
-                    System.out.println("crystal receiving energy");
+                    MageCrystalBlockEntity crystalToPower = null;
+                    if (direction == Direction.NORTH) {
+                        if (world.getBlockEntity(pos.south()) instanceof MageCrystalBlockEntity) {
+                            crystalToPower = (MageCrystalBlockEntity) world.getBlockEntity(pos.south());
+                        }
+                    }
+                    else if (direction == Direction.EAST) {
+                        if (world.getBlockEntity(pos.west()) instanceof MageCrystalBlockEntity) {
+                            crystalToPower = (MageCrystalBlockEntity) world.getBlockEntity(pos.west());
+                        }
+                    }
+                    else if (direction == Direction.WEST) {
+                        if (world.getBlockEntity(pos.east()) instanceof MageCrystalBlockEntity) {
+                            crystalToPower = (MageCrystalBlockEntity) world.getBlockEntity(pos.east());
+                        }
+                    }
+                    else {
+                        if (world.getBlockEntity(pos.north()) instanceof MageCrystalBlockEntity) {
+                            crystalToPower = (MageCrystalBlockEntity) world.getBlockEntity(pos.north());
+                        }
+                    }
+                    if (crystalToPower != null) {
+                        if (crystalBlock.ticksLeft > 0) {
+                            crystalToPower.increaseRadius();
+                            if (crystalToPower.ticksLeft == 0) {
+                                //I'm not sure why this doesn't update visually
+                                world.setBlockState(crystalToPower.getPos(), world.getBlockState(crystalToPower.getPos()).with(MageCrystalBlock.ISACTIVE, true));
+                            }
+                            crystalToPower.ticksLeft++;
+                        }
+                    }
                 }
             }
         }
